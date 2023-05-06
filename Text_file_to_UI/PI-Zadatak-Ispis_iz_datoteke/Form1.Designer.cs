e6a0878b29e9d94c9961d3712dc6891f86fbefa0
Marin Orejaš <morejas21@foi.hr>
2023-05-06 16:45:13 +0200

    Choose file button #1

--- Text_file_to_UI/PI-Zadatak-Ispis_iz_datoteke/Form1.Designer.cs
+++ Text_file_to_UI/PI-Zadatak-Ispis_iz_datoteke/Form1.Designer.cs
@@ -28,13 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnChooseFile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnChooseFile
+            // 
+            this.btnChooseFile.Location = new System.Drawing.Point(30, 28);
+            this.btnChooseFile.Name = "btnChooseFile";
+            this.btnChooseFile.Size = new System.Drawing.Size(154, 46);
+            this.btnChooseFile.TabIndex = 0;
+            this.btnChooseFile.Text = "Choose File...";
+            this.btnChooseFile.UseVisualStyleBackColor = true;
+            this.btnChooseFile.Click += new System.EventHandler(this.btnChooseFile_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(410, 481);
+            this.Controls.Add(this.btnChooseFile);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnChooseFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- Text_file_to_UI/PI-Zadatak-Ispis_iz_datoteke/Form1.cs
+++ Text_file_to_UI/PI-Zadatak-Ispis_iz_datoteke/Form1.cs
@@ -16,5 +16,22 @@ namespace PI_Zadatak_Ispis_iz_datoteke
         {
             InitializeComponent();
         }
+
+        private void btnChooseFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Title = "Choose File...";
+            openFileDialog1.Filter = "Text Files|*.txt";
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileDestination = openFileDialog1.FileName;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
